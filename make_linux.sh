a3102162fb0a37e107dd12f41a82d0a1dbec5ee4
Dan Smith <drsmith900@gmail.com>
2014-07-23 11:20:00 +1000

    linux compile and make scripts added

---
+++ make_linux.sh
@@ -0,0 +1,2 @@
+rm ./src/com/company/*.class
+javac ./src/com/company/*.java
---
+++ run_linux.sh
@@ -0,0 +1,5 @@
+cd src
+echo "Please enter two chess tiles (eg. A8 B7):"
+read INPUT
+java com.company.Main $INPUT
+cd ..
